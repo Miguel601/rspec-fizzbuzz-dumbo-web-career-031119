3683c837387da7446b8ce578e792d13038ab557d
Miguel Henriquez <henriquez.miguel@outlook.com>
2019-02-28 18:58:59 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+class Fizzbuzz
+  def calculate number
+    if number % 3 == 0 && number % 5 == 0
+      'fizzbuzz'
+    elsif number % 3 == 0
+      'fizz'
+    elsif number % 5 == 0
+      'buzz'
+    else
+      number
+    end
+  end
+end
